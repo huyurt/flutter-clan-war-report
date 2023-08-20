8e79590cac8d51f08fa66529ae5d7757f9d72e17
Hudayfe Yurt <hudayfeyurt@hotmail.com>
2023-08-20 23:03:47 +0300

    Bugfix scroll for app info screen

--- lib/ui/screens/setting_screen/about_app_screen.dart
+++ lib/ui/screens/setting_screen/about_app_screen.dart
@@ -22,68 +22,85 @@ class AboutAppScreen extends StatelessWidget {
             final packageInfo = snapshot.data;
             return Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 24.0),
-                  child: Text(
-                    packageInfo?.appName ?? '',
-                    style: const TextStyle(fontSize: 16.0),
-                  ),
-                ),
-                FadeIn(
-                  animate: true,
-                  duration: const Duration(milliseconds: 250),
-                  child: Container(
-                    padding: const EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(50.0),
-                    ),
-                    child: Image.asset(
-                      AppConstants.appIconImage,
-                      height: 32.0,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 24.0),
-                  child: Column(
+                Expanded(
+                  child: ListView(
+                    key: PageStorageKey(key),
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
                     children: [
-                      Text(
-                        tr(LocaleKey.aboutAppShortMessage),
-                        style: const TextStyle(color: Colors.grey),
-                        textAlign: TextAlign.center,
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 24.0),
+                        child: Text(
+                          packageInfo?.appName ?? '',
+                          style: const TextStyle(fontSize: 16.0),
+                        ),
                       ),
-                      const Text(
-                        AppConstants.supercellFanContentPolicyUrl,
-                        style: TextStyle(color: Colors.grey),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 16.0),
-                  child: Text(
-                      '${tr(LocaleKey.version)}: ${packageInfo?.version ?? ''}'),
-                ),
-                Text(tr(LocaleKey.poweredByFlutter)),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 24.0),
-                  child: ExpansionTile(
-                    initiallyExpanded: true,
-                    title: Text(
-                      tr(LocaleKey.aboutAppQuestion1),
-                      style: const TextStyle(fontSize: 12.0),
-                    ),
-                    children: [
-                      ListTile(
-                        title: Text(
-                          tr(LocaleKey.aboutAppAnswer1),
-                          style: const TextStyle(
-                            color: Colors.grey,
-                            fontSize: 12.0,
+                      FadeIn(
+                        animate: true,
+                        duration: const Duration(milliseconds: 250),
+                        child: Center(
+                          child: Container(
+                            height: 64.0,
+                            width: 64.0,
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(50.0),
+                            ),
+                            child: Image.asset(
+                              AppConstants.appIconImage,
+                              height: 32.0,
+                              fit: BoxFit.fitHeight,
+                            ),
                           ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 24.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              tr(LocaleKey.aboutAppShortMessage),
+                              style: const TextStyle(color: Colors.grey),
+                              textAlign: TextAlign.center,
+                            ),
+                            const Text(
+                              AppConstants.supercellFanContentPolicyUrl,
+                              style: TextStyle(color: Colors.grey),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: Text(
+                          '${tr(LocaleKey.version)}: ${packageInfo?.version ?? ''}',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Text(
+                        tr(LocaleKey.poweredByFlutter),
+                        textAlign: TextAlign.center,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 24.0),
+                        child: ExpansionTile(
+                          initiallyExpanded: true,
+                          title: Text(
+                            tr(LocaleKey.aboutAppQuestion1),
+                            style: const TextStyle(fontSize: 12.0),
+                          ),
+                          children: [
+                            ListTile(
+                              title: Text(
+                                tr(LocaleKey.aboutAppAnswer1),
+                                style: const TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 12.0,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
